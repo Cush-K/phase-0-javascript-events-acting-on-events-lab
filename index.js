834a1309f27b39e2393760cd7c1e7b865e629a80
Kalvine <kkalvine4@gmail.com>
2024-07-07 14:41:32 +0300

    Moves Dodger left, right, up & down

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ function moveDodgerLeft() {
     function moveDodgerRight() {
             const rightNumbers = dodger.style.left.replace(`px`, ``);
             const right = parseInt(rightNumbers, 10);
-            const leftNumbers = dodger.style.left.replace(`px`, ``);
-            const left = parseInt(leftNumbers, 10);
             
     
-            if (left < 360) {
-            dodger.style.left = `${left + 1}px`;
+            if (right < 360) {
+            dodger.style.left = `${right + 1}px`;
             }};
     
             document.addEventListener("keydown", function (e) {
@@ -31,3 +29,28 @@ function moveDodgerLeft() {
                 }
               });
   
+              function moveDodgerDown () {
+                const bottomNumbers = dodger.style.bottom.replace("px", "");
+                const bottom = parseInt(bottomNumbers, 10);
+        
+                if (bottom >0){
+                  dodger.style.bottom = `${bottom-1}px`;
+                }};
+              document.addEventListener("keydown", (e)=> {
+                if (e.key === "ArrowDown"){
+                  moveDodgerDown();
+                }
+              });
+
+              function moveDodgerUp () {
+                const topNumbers = dodger.style.bottom.replace("px", ``);
+                const top = parseInt(topNumbers, 10);
+
+                if (top <380){
+                  dodger.style.bottom = `${top+1}px`;
+                }};
+              document.addEventListener("keydown", (e)=> {
+                if (e.key === "ArrowUp"){
+                  moveDodgerUp();
+                }
+              });
